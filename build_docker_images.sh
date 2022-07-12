bb921f9f4bea4a62d0e6047fa5fee7d56dd953ec
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-07-12 15:21:06 -0700

    Update build_docker_images.sh
    Remove the message to navigate to the root of the repo, this can be automated.

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -6,23 +6,14 @@ if [ "$EUID" -ne 0 ]; then
     echo ""
 fi
 
-if [ -z "$NOSLEEP" ]; then
-    echo "Assuming you are in the root directory of the repository"
-    echo "Press CTRL+C now if this is not the case, and re-run this script from the root directory of the repository"
-    sleep 5
-    echo "Continuing..."
-fi
-
-ORIG_DIR="$(pwd)"
-
 function build_container () {
     WIDGET_NAME=$1
     DOCKER_IMAGE=$2
     DOCKER_TAG=$3
 
-    cd "clij_benchmarking_workflow/widgets/clij_benchmarking_workflow/$WIDGET_NAME/Dockerfiles"
+    pushd "`dirname $0`/clij_benchmarking_workflow/widgets/clij_benchmarking_workflow/$WIDGET_NAME/Dockerfiles" > /dev/null
     docker build -t "$DOCKER_IMAGE:$DOCKER_TAG" .
-    cd "$ORIG_DIR"
+    popd > /dev/null
 }
 
 build_container "jupyter_stats" "biodepot/jupyter-stats" \
